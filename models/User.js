297e567e2caa32c53791a492bbb9acea6ee06116
gsus-tech <chucho.moralitos0106@gmail.com>
2025-03-25 11:03:40 -0700

    Users schema added

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['active', 'inactive', 'suspended'],
+  },
+  verfied: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  lastLogin: {
+    type: Date,
+    default: null,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
